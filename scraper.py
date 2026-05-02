7cd3d997c87831796839931146494e3b2ad906e5
social-media-automation <aaservices1240@gmail.com>
2026-05-02 15:08:38 +0500

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,16 @@
+def get_jobs():
+    jobs = [
+        {
+            "title": "Software Engineer",
+            "company": "Gov Department",
+            "location": "Pakistan",
+            "link": "https://example.com"
+        },
+        {
+            "title": "Data Analyst",
+            "company": "Private Company",
+            "location": "Lahore",
+            "link": "https://example.com"
+        }
+    ]
+    return jobs
